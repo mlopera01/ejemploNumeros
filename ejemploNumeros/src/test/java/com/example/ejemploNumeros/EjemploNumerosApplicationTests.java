261bf8f351798e084a05874599145c7f6f6cd368
mlopera01 <miramaandrea@gmail.com>
2022-11-01 10:36:40 -0500

    Con pruebas unitarias

--- ejemploNumeros/src/test/java/com/example/ejemploNumeros/EjemploNumerosApplicationTests.java
+++ ejemploNumeros/src/test/java/com/example/ejemploNumeros/EjemploNumerosApplicationTests.java
@@ -1,13 +1,62 @@
 package com.example.ejemploNumeros;
 
+import com.example.ejemploNumeros.controller.ResultadoCalculo;
+import com.example.ejemploNumeros.service.NumerosService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EjemploNumerosApplicationTests {
+	@Autowired
+	private NumerosService numeroService;
 
 	@Test
-	void contextLoads() {
+	void testVerificarSiEsNumeroMenor() {
+		//List<Integer> li = new List<Integer> ();
+		//li.add(3);
+		List<Integer> li = Arrays.asList(3,0,-1,50);
+		ResultadoCalculo rc = new ResultadoCalculo ();
+		rc = numeroService.devolverCalculo(li);
+		assertEquals(-1,rc.getNumeroMenor());
 	}
+
+	@Test
+	void testVerificarSiEsMayor() {
+		List<Integer> li = Arrays.asList(3,0,-1,50);
+		ResultadoCalculo rc = new ResultadoCalculo ();
+		rc = numeroService.devolverCalculo(li);
+		assertEquals(50,rc.getNumeroMayor());
+	}
+	@Test
+	void testVerificarSumaPositivos() {
+		List<Integer> li = Arrays.asList(3,0,-1,50);
+		ResultadoCalculo rc = new ResultadoCalculo ();
+		rc = numeroService.devolverCalculo(li);
+		assertEquals(53,rc.getSumaPositivos());
+	}
+
+	@Test
+	void testVerificarSumaNegativos() {
+		List<Integer> li = Arrays.asList(3,0,-1,50);
+		ResultadoCalculo rc = new ResultadoCalculo ();
+		rc = numeroService.devolverCalculo(li);
+		assertEquals(-1,rc.getSumaNegativos());
+	}
+
+	@Test
+	void testVerificarSumaTotales() {
+		List<Integer> li = Arrays.asList(3,0,-1,50);
+		ResultadoCalculo rc = new ResultadoCalculo ();
+		rc = numeroService.devolverCalculo(li);
+		assertEquals(52,rc.getSumaNumeros());
+	}
+
 
 }
